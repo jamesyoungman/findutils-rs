1befe59759354936ee2535e51813ea9e23eaadae
James Youngman <james@youngman.org>
2024-06-09 19:56:38 +0100

    Correctly parse "find -P -print".

--- src/bin/find/options.rs
+++ src/bin/find/options.rs
@@ -1,7 +1,7 @@
 use std::collections::VecDeque;
 use std::fmt::Write;
 
-use getopt::Opt;
+use getopt::{ErrorKind, Opt};
 
 use super::errors::UsageError;
 
@@ -26,6 +26,7 @@ pub fn parse_options(mut args: VecDeque<&str>) -> Result<(Options, VecDeque<&str
     let args_for_opt_parser: Vec<String> = args.iter().map(|s| s.to_string()).collect();
     let mut opts = getopt::Parser::new(&args_for_opt_parser, "HLP"); // E not yet implemented.
     let mut traversal = TraversalMode::P;
+    let mut need_unshift = false;
     // Process the leading options.
     loop {
         match opts.next() {
@@ -39,139 +40,178 @@ pub fn parse_options(mut args: VecDeque<&str>) -> Result<(Options, VecDeque<&str
             Some(Err(e)) => {
                 let mut msg = String::new();
                 write!(msg, "{}", e).expect("writes to string always succeeds");
-                return Err(UsageError(msg));
+                let kind = e.kind();
+                if kind == ErrorKind::UnknownOption {
+                    // We come here when we have a command line like
+                    // "find -L -print" because 'p' is not a known
+                    // option letter.
+                    //
+                    // We benefit I suppose from the fact that none of
+                    // the valid option letters is also the first
+                    // character of a predicate.
+                    need_unshift = true;
+                } else {
+                    return Err(UsageError(msg));
+                }
             }
         }
     }
-    args.drain(..opts.index());
+    let limit = opts.index() - if need_unshift { 1 } else { 0 };
+    args.drain(..limit);
     let opts = Options { traversal };
     Ok((opts, args))
 }
 
 #[cfg(test)]
-fn parse_opt_vec(mut args: Vec<&str>) -> Result<(Options, Vec<&str>), UsageError> {
-    args.insert(0, "find"); // argv[0]
-    let dq: VecDeque<&str> = args.iter().copied().collect();
-    match parse_options(dq) {
-        Err(e) => Err(e),
-        Ok((opts, args)) => {
-            let v: Vec<&str> = args.iter().copied().collect();
-            Ok((opts, v))
+mod tests {
+    use super::*;
+
+    fn parse_opt_vec(mut args: Vec<&str>) -> Result<(Options, Vec<&str>), UsageError> {
+        args.insert(0, "find"); // argv[0]
+        let dq: VecDeque<&str> = args.iter().copied().collect();
+        match parse_options(dq) {
+            Err(e) => Err(e),
+            Ok((opts, args)) => {
+                let v: Vec<&str> = args.iter().copied().collect();
+                Ok((opts, v))
+            }
         }
     }
-}
 
-#[test]
-fn parse_otions_no_options() {
-    assert_eq!(
-        Ok((
+    #[test]
+    fn parse_otions_no_options() {
+        assert_eq!(
+            Ok((
+                Options {
+                    traversal: TraversalMode::P,
+                },
+                vec![]
+            )),
+            parse_opt_vec(Vec::new())
+        );
+    }
+
+    #[test]
+    fn parse_otions_p() {
+        assert_eq!(
+            Ok((
+                Options {
+                    traversal: TraversalMode::P,
+                },
+                vec![]
+            )),
+            parse_opt_vec(vec!["-P"])
+        );
+    }
+
+    #[test]
+    fn parse_otions_hp() {
+        assert_eq!(
+            Ok((
+                Options {
+                    traversal: TraversalMode::P,
+                },
+                vec![]
+            )),
+            parse_opt_vec(vec!["-HP"])
+        );
+    }
+
+    #[test]
+    fn parse_otions_lp() {
+        assert_eq!(
+            Ok((
+                Options {
+                    traversal: TraversalMode::P,
+                },
+                vec![]
+            )),
+            parse_opt_vec(vec!["-LP"])
+        );
+    }
+
+    #[test]
+    fn parse_otions_l() {
+        let expected = Ok((
             Options {
-                traversal: TraversalMode::P,
+                traversal: TraversalMode::L,
             },
-            vec![]
-        )),
-        parse_opt_vec(Vec::new())
-    );
-}
+            vec![],
+        ));
+        assert_eq!(expected, parse_opt_vec(vec!["-L"]));
+        assert_eq!(expected, parse_opt_vec(vec!["-LL"]));
+        assert_eq!(expected, parse_opt_vec(vec!["-HL"]));
+        assert_eq!(expected, parse_opt_vec(vec!["-PL"]));
+        assert_eq!(expected, parse_opt_vec(vec!["-P", "-L"]));
+    }
 
-#[test]
-fn parse_otions_p() {
-    assert_eq!(
-        Ok((
-            Options {
-                traversal: TraversalMode::P,
-            },
-            vec![]
-        )),
-        parse_opt_vec(vec!["-P"])
-    );
-}
+    #[test]
+    fn parse_otions_h() {
+        assert_eq!(
+            Ok((
+                Options {
+                    traversal: TraversalMode::H,
+                },
+                vec![]
+            )),
+            parse_opt_vec(vec!["-H"])
+        );
+    }
 
-#[test]
-fn parse_otions_hp() {
-    assert_eq!(
-        Ok((
-            Options {
-                traversal: TraversalMode::P,
-            },
-            vec![]
-        )),
-        parse_opt_vec(vec!["-HP"])
-    );
-}
-
-#[test]
-fn parse_otions_lp() {
-    assert_eq!(
-        Ok((
-            Options {
-                traversal: TraversalMode::P,
-            },
-            vec![]
-        )),
-        parse_opt_vec(vec!["-LP"])
-    );
-}
-
-#[test]
-fn parse_otions_l() {
-    let expected = Ok((
-        Options {
-            traversal: TraversalMode::L,
-        },
-        vec![],
-    ));
-    assert_eq!(expected, parse_opt_vec(vec!["-L"]));
-    assert_eq!(expected, parse_opt_vec(vec!["-LL"]));
-    assert_eq!(expected, parse_opt_vec(vec!["-HL"]));
-    assert_eq!(expected, parse_opt_vec(vec!["-PL"]));
-    assert_eq!(expected, parse_opt_vec(vec!["-P", "-L"]));
-}
-
-#[test]
-fn parse_otions_h() {
-    assert_eq!(
-        Ok((
+    #[test]
+    fn parse_otions_double_dash() {
+        let expected = Ok((
             Options {
                 traversal: TraversalMode::H,
             },
-            vec![]
-        )),
-        parse_opt_vec(vec!["-H"])
-    );
-}
+            vec![],
+        ));
+        assert_eq!(expected, parse_opt_vec(vec!["-H"]));
+        assert_eq!(expected, parse_opt_vec(vec!["-H", "--"]));
+    }
 
-#[test]
-fn parse_otions_double_dash() {
-    let expected = Ok((
-        Options {
-            traversal: TraversalMode::H,
-        },
-        vec![],
-    ));
-    assert_eq!(expected, parse_opt_vec(vec!["-H"]));
-    assert_eq!(expected, parse_opt_vec(vec!["-H", "--"]));
-}
+    #[test]
+    fn parse_otions_non_options() {
+        let expected = Ok((
+            Options {
+                traversal: TraversalMode::P,
+            },
+            vec!["foo/"],
+        ));
+        assert_eq!(expected, parse_opt_vec(vec!["foo/"]));
+        assert_eq!(expected, parse_opt_vec(vec!["--", "foo/"]));
+    }
 
-#[test]
-fn parse_otions_non_options() {
-    let expected = Ok((
-        Options {
-            traversal: TraversalMode::P,
-        },
-        vec!["foo/"],
-    ));
-    assert_eq!(expected, parse_opt_vec(vec!["foo/"]));
-    assert_eq!(expected, parse_opt_vec(vec!["--", "foo/"]));
-}
+    #[test]
+    fn parse_otions_with_start_and_pred() {
+        let expected = Ok((
+            Options {
+                traversal: TraversalMode::L,
+            },
+            vec!["foo/", "-print"],
+        ));
+        assert_eq!(expected, parse_opt_vec(vec!["-L", "foo/", "-print"]));
+    }
 
-#[test]
-fn parse_otions_with_start_and_pred() {
-    let expected = Ok((
-        Options {
-            traversal: TraversalMode::L,
-        },
-        vec!["foo/", "-print"],
-    ));
-    assert_eq!(expected, parse_opt_vec(vec!["-L", "foo/", "-print"]));
+    #[test]
+    fn parse_otions_with_doubledash_pred_but_no_start() {
+        let expected = Ok((
+            Options {
+                traversal: TraversalMode::L,
+            },
+            vec!["-print"],
+        ));
+        assert_eq!(expected, parse_opt_vec(vec!["-L", "--", "-print"]));
+    }
+
+    #[test]
+    fn parse_otions_with_pred_but_no_start() {
+        let expected = Ok((
+            Options {
+                traversal: TraversalMode::L,
+            },
+            vec!["-print"],
+        ));
+        assert_eq!(expected, parse_opt_vec(vec!["-L", "-print"]));
+    }
 }
